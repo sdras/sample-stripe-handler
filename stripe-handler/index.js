328b47314548f4e311fb248cd60a2ce570af46d5
sdras <sarah.drasner@gmail.com>
2017-11-25 15:06:22 -0700

    add in stripe index.js

---
+++ stripe-handler/index.js
@@ -0,0 +1,36 @@
+import stripePackage from 'stripe';
+const stripe = stripePackage('sk_test_js5LRkmS7OsYHtVc7XYvK9OB');
+
+module.exports = function(context, req) {
+  context.log('we starting to get down');
+
+  //if we have a request body, an email, and a token, let's get started
+  if (req.body && req.body.stripeEmail && req.body.stripeToken) {
+    stripe.customers
+      .create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+      })
+      .then(customer =>
+        stripe.charges.create({
+          amount,
+          description: 'Sample Charge',
+          currency: 'usd',
+          customer: customer.id
+        })
+      )
+      .then(charge => {
+        context.res = {
+          // status: 200
+          body: 'This has been completed'
+        };
+        context.done();
+      });
+  } else {
+    context.res = {
+      status: 400,
+      body: "We're missing something"
+    };
+    context.done();
+  }
+};
